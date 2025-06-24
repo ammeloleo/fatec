936e5d53ae7a8f8412519b0f3cb233a619832b32
Leonardo Andrade <leonardoammelo@gmail.com>
2025-06-24 03:05:56 -0300

    Add files via upload

---
+++ Portifolio/Js/json.js
@@ -0,0 +1,9 @@
+function botaoClique(click){
+    let botaoClique = document.getElementById('linkedinBottom')
+    window.location.href = 'https://www.linkedin.com/in/leonardo-andrade-0541b4270/' 
+}
+
+function bottomSeeMore (click){
+    let  bottomSeeMore = document.getElementById('seeMore')
+    window.location.href = 'https://github.com/ammeloleo'
+}
